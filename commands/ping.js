3587a4d9238ab95c01378064bdb24e4b89a0ffc8
Flam3rboy <samuel.scheit@gmail.com>
2019-04-20 21:46:12 +0200

    bux fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,25 +1,10 @@
 module.exports.run = (msg, args) => {
-    console.log("ping");
-    
-    return message.channel.send("Der Ping des Bots beträgt: "+bot.ping + " ms.");
+	console.log("ping");
+
+	return msg.channel.send("Der Ping des Bots beträgt: " + bot.ping + " ms.");
 };
 
 module.exports.help = {
 	help: "",
 	description: "Ping"
 };
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
