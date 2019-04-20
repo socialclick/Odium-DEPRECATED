06d85e126a347d34c90ac8540077c9697e1decd8
unknown <matteogropp@gmail.com>
2019-04-20 21:35:53 +0200

    general changes + welcome msg added

--- extra/welcome.js
+++ extra/welcome.js
@@ -1,3 +1,17 @@
 module.exports.init = () => {
 	console.log("Welcome initalized");
+
+	client.on("guildMemberAdd", member => {
+		const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+		
+		let owner = member.guild.owner
+
+		if (!channel){
+			send(owner, "error","Welcome Message", "welcome channel does not exist, please create one!");
+
+			return console.log("Cnl not found")
+		};
+	
+		channel.send(`Welcome to the server, ${member}`);
+	})
 };
