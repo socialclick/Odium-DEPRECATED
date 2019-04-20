e18e796d45082537999b47d9261be0c48f837fa0
unknown <matteogropp@gmail.com>
2019-04-20 21:39:54 +0200

    ping command added

---
+++ commands/ping.js
@@ -0,0 +1,25 @@
+module.exports.run = (msg, args) => {
+    console.log("ping");
+    
+    return message.channel.send("Der Ping des Bots beträgt: "+bot.ping + " ms.");
+};
+
+module.exports.help = {
+	help: "",
+	description: "Ping"
+};
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
