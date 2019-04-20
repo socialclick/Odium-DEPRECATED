ab40c164330a9c2717b405bf9063a8a9981c67a7
Flam3rboy <samuel.scheit@gmail.com>
2019-04-20 20:21:44 +0200

    bug fix

---
+++ main.js
@@ -0,0 +1,27 @@
+var Discord = require("discord.js");
+global.client = new Discord.Client();
+var fs = require("fs");
+var config = require("./botconfig.json");
+var basic = require("./basic");
+
+client.commands = new Discord.Collection();
+
+client.on("ready", () => {
+	basic.init();
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", message => {
+	if (message.author.bot) return;
+	if (message.channel.type === "dm")
+		return message.channel.send(
+			"Die Befehle dieses Bots sind nicht für Privatnachrichten ausgelegt"
+		);
+
+	let prefix = Prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0].toLowerCase();
+	let args = messageArray.slice(1);
+});
+
+client.login(config.token);
