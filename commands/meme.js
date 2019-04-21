00f3057b42350c3164030db1108f6b4ff3cbd64b
unknown <matteogropp@gmail.com>
2019-04-21 18:14:19 +0200

    added memes

---
+++ commands/meme.js
@@ -0,0 +1,28 @@
+module.exports.run = (msg, args) => {
+	console.log("meme started");
+    //command
+
+    let reddit =[
+        "dankmemes",
+        "ich_iel"
+    ]
+
+    let num = Math.floor(Math.random())
+    let subreddit = reddit[num]
+    let reddit =[
+        "dankmemes",
+        "ich_iel"
+    ]
+
+    let num = Math.floor(Math.random())
+    let subreddit = reddit[num]
+    
+    message.channel.startTyping();
+    randomPuppy(subreddit).then(async url => {
+        await message.channel.send(url).then(() => message.channel.stopTyping());
+    }).catch(err => console.error(err));
+};
+module.exports.help = {
+	help: "",
+	description: "Shows memes from r/dankmemes r/ich_iel"
+};
