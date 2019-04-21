6574761be11cecce23682fe07c77899ebe448dc9
unknown <matteogropp@gmail.com>
2019-04-21 17:40:41 +0200

    Finished Leave.js msg

--- extra/leave.js
+++ extra/leave.js
@@ -1,8 +1,26 @@
-module.exports.run = (msg, args) => {
+module.exports.init = (msg, args) => {
 	console.log("leave.js");
 
-    
+    client.on("guildMemberRemove", member => {
+        const channel = member.guild
+        ._sortedChannels()
+        .find(ch => ch.type === "text");
 
+    let owner = member.guild.owner;
+
+    if (!channel) {
+        send(
+            owner,
+            "error",
+            "Welcome Message",
+            "welcome channel does not exist, please create one!"
+        );
+
+        return console.log("Cnl not found");
+    }
+
+    channel.send(`${member}, Leaved the Server`);
+});
 };
 
 module.exports.help = {
