3be31a415bbad362013ffbd2c9ee9a8a1661a667
unknown <matteogropp@gmail.com>
2019-04-22 17:27:57 +0200

    added clear cmd

--- commands/clear.js
+++ commands/clear.js
@@ -8,7 +8,25 @@ module.exports.run = (msg, args) => {
         message.channel.bulkDelete(args[0]).then(() => {
             message.channel.send(`Cleared ${args[0]} Messages!`).then(msg => msg.delete(3000));
         });
-}
+
+        let clearEmbed = new Discord.RichEmbed()
+        .setDescription("~clear~")
+        .setColor("#7289DA")
+        .addField("Deleted Msg",`${args[0]}`)
+        .addField("Cleared by",`<@${message.author.id}>`)
+        .addField("Cleared in",  message.channel)
+        .addField("time", message.createdAt)
+        .setFooter(
+			client.user.username + " Bot coded by NaCl-y#4400 & Flam3rboy#5979",
+			client.user.displayAvatarURL
+		);
+    
+        let clearhannel = message.guild.channels.find(`name`, "report");
+        if(!clearchannel) return message.guild.createChannel("report", "text");
+
+        clearchannel.send(clearEmbed)
+    
+};
 
 module.exports.help = {
 	help: "",
