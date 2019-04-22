e6dc2d3f38de2718aba1de43935a4ef55f214baf
unknown <matteogropp@gmail.com>
2019-04-22 20:35:13 +0200

    fixed bugs

--- commands/report.js
+++ commands/report.js
@@ -2,7 +2,32 @@ module.exports.run = (msg, args) => {
 	console.log("report");
 	//command
 	//report
-
+	if(!args[1]){
+		let rUser = msg.guild.member(
+			msg.mentions.users.first() || msg.guild.members.get(args[0])
+		);
+		if (!rUser) return msg.channel.send("Couldn´t find user.");
+		let reason = args.join(" ").slice(22);
+	
+		let reportEmbed = new Discord.RichEmbed()
+			.setDescription("~Report~")
+			.setColor("#ff6600")
+			.addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+			.addField("Reported by", `${msg.author} with ID ${msg.author.id}`)
+			.addField("channel", msg.channel)
+			.addField("time", msg.createdAt)
+			.setFooter(
+				client.user.username + " Bot coded by NaCl-y#4400 & Flam3rboy#5979",
+				client.user.displayAvatarURL
+			);
+	
+		let reportschannel = msg.guild.channels.find(`name`, "report");
+		if (!reportschannel) return msg.guild.createChannel("report", "text");
+	
+		msg.delete().catch(O_o => {});
+		reportschannel.send(reportEmbed);
+	};
+	
 	let rUser = msg.guild.member(
 		msg.mentions.users.first() || msg.guild.members.get(args[0])
 	);
@@ -30,6 +55,6 @@ module.exports.run = (msg, args) => {
 };
 
 module.exports.help = {
-	help: "",
-	description: ""
+	help: "report",
+	description: "reports a user ``!report [@user] [reason]"
 };
--- commands/warn.js
+++ commands/warn.js
@@ -2,6 +2,33 @@ module.exports.run = (msg, args) => {
 	console.log("warn");
 	//command
 	//report
+	if(!args[1]){	
+	let rUser = msg.guild.member(
+		msg.mentions.users.first() || msg.guild.members.get(args[0])
+	);
+	if (!rUser) return msg.channel.send("Couldn´t find user.");
+	let reason = args.join(" ").slice(22);
+
+	let reportEmbed = new Discord.RichEmbed()
+		.setDescription("~Warn~")
+		.setColor("#ff6600")
+		.addField("Warned User", `${rUser} with ID: ${rUser.id}`)
+		.addField("Warned by", `${msg.author} with ID ${msg.author.id}`)
+		.addField("channel", msg.channel)
+		.addField("time", msg.createdAt)
+		.setFooter(
+			client.user.username + " Bot coded by NaCl-y#4400 & Flam3rboy#5979",
+			client.user.displayAvatarURL
+		);
+
+	let warnchannel = msg.guild.channels.find(`name`, "report");
+	if (!warnchannel) return msg.guild.createChannel("report", "text");
+
+	msg.delete().catch(O_o => {});
+	reportschannel.send(reportEmbed);
+	return
+	};
+
 	let rUser = msg.guild.member(
 		msg.mentions.users.first() || msg.guild.members.get(args[0])
 	);
@@ -29,6 +56,6 @@ module.exports.run = (msg, args) => {
 };
 
 module.exports.help = {
-	help: "!warn [@user] [reason]",
-	description: "warn`s a user"
+	help: "warn",
+	description: "warn`s a user ``!warn [@user] [reason]"
 };
