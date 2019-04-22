4b6a9a93064a5772ae96eca700094afd27ef9d28
unknown <matteogropp@gmail.com>
2019-04-22 17:13:39 +0200

    added warn, fixed bugs in leave & welcome

--- extra/welcome.js
+++ extra/welcome.js
@@ -6,8 +6,6 @@ module.exports.init = () => {
 			._sortedChannels()
 			.find(ch => ch.type === "text");
 
-		let owner = member.guild.owner;
-
 		if (!channel) {
 			return message.guild.createChannel("welcome", "text");
 		};
