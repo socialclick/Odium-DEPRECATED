b46f3d5cadd1714fde9f7c36b2f78db1dc588a49
Flam3rboy <samuel.scheit@gmail.com>
2019-04-22 16:31:18 +0200

    help + updated web + bug fix

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var app;
 
-module.exports.init = express => {
+module.exports.init = d => {
+	var express = d.express;
+	var http = d.http;
+	var request = d.request;
 	app = express();
 
-	app.use("/", express.static("static"));
+	app.use("/", (req, res) => {
+		request("http://localhost:3000/" + req.originalUrl).pipe(res);
+	});
+
+	app.get("/api/", (req, res) => {});
 
 	app.listen(1008, "0.0.0.0");
 };
