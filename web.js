8c6b83e47420f972bae6e2e2d9acb485b343c6c0
Flam3rboy <samuel.scheit@gmail.com>
2019-04-23 12:36:14 +0200

    update web

--- web.js
+++ web.js
@@ -1,34 +1,24 @@
-var app;
+const express = require("express");
+const request = require("request");
+const app = express();
 var port = 1008;
+const server = app.listen(port, () => {
+	console.log("Listening on port: " + port);
+});
+global.io = require("socket.io")(server);
 
-module.exports.init = d => {
-	var express = d.express;
-	var http = d.http;
-	var request = d.request;
-	var io = new d.socket({
-		path: "//socket.io",
-		serveClient: false,
-		pingInterval: 10000,
-		pingTimeout: 5000,
-		cookie: false
-	});
+app.get("/api/authorize", (req, res) => {
+	console.log(req.query.code);
+	res.send("<script>window.closes()</script>");
+});
 
-	app = express();
+io.on("connection", function(socket) {
+	console.log("an user connected", socket);
+	socket.emit("hi", "test");
+});
 
-	app.get("/api/authorize", (req, res) => {
-		console.log(req.query.code);
-		res.send("<script>window.close()</script>");
-	});
+app.use("/", (req, res) => {
+	request("http://localhost:3000/" + req.originalUrl).pipe(res);
+});
 
-	io.on("connection", function(socket) {
-		console.log("an user connected");
-	});
-
-	app.use("/", (req, res) => {
-		request("http://localhost:3000/" + req.originalUrl).pipe(res);
-	});
-
-	app.listen(port, () => {
-		console.log(`Example app listening on port ${port}!`);
-	});
-};
+module.exports.init = d => {};
