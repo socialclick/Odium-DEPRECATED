984cda8b2719986968c7e5dad631e60e66de9b1f
Flam3rboy <samuel.scheit@gmail.com>
2019-04-23 20:07:18 +0200

    web

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
 	}
 });
 
-function login() {
+function doLogin() {
 	var w = window.open(
 		"https://discordapp.com/api/oauth2/authorize?client_id=569201129691283497&redirect_uri=http://flam3rboy.ddns.net/api/authorize&response_type=code&scope=identify guilds&state=" +
 			socket.id,
